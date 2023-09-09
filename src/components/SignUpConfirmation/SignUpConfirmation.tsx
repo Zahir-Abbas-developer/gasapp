ea0dabcefdb2687c9ea4be995149a242e775f633
zahir <zahirr060@gmail.com>
2023-09-08 22:37:46 -0700

    #commit updated code

--- src/components/SignUpConfirmation/SignUpConfirmation.tsx
+++ src/components/SignUpConfirmation/SignUpConfirmation.tsx
@@ -6,13 +6,13 @@ const ConfirmationSignUp = () => {
  
     return (
        <Row style={{marginTop:"120px",textAlign:"center"}}>
-        <Col sm={24} style={{textAlign:"center"}}>
+        <Col xs={24} style={{textAlign:"center"}}>
         <img src={confirmationImgae} height={176}  width={176}/>
         </Col>
-        <Col sm={24} style={{textAlign:"center",marginTop:"40px"}} >
+        <Col xs={24} style={{textAlign:"center",marginTop:"40px"}} >
         <p>Thank you for Signing up with Company Name</p>
         </Col>
-        <Col sm={24} style={{textAlign:"center",marginTop:"40px"}} >
+        <Col xs={24} style={{textAlign:"center",marginTop:"40px"}} >
        <Link to="/login">
        <Button
                   type="primary"
